f3ac8e5825446f0aff934bb13f562a2efffe6fc1
Karan <58234870+kkara9009@users.noreply.github.com>
2023-11-20 15:30:19 +0530

    Create desProjects.js

---
+++ src/utils/desProjects.js
@@ -0,0 +1,192 @@
+const bowsette = {
+    title: 'Bowsette',
+    description: 'Expanding on the "Bowsette" meme, except now everyone is a Peach',
+    typeOfWork: 'Personal Project',
+    galleryImgUrl: 'https://flic.kr/p/2kQfyJH',
+    image: '/assets/images/projects/designProjects/bowsette.jpg',
+    webpImage: '/assets/images/projects/designProjects/bowsette.webp',
+    key: 0
+}
+
+const psike = {
+    title: 'Psiké',
+    description: 'Business card and logo design for child psychologist "Sue-Tayna"',
+    typeOfWork: 'Business Card',
+    galleryUrl: 'https://flic.kr/s/aHsmUEPSgt',
+    image: '/assets/images/projects/designProjects/psike.jpg',
+    webpImage: '//assets/images/projects/designProjects/psike.webp',
+    key: 1
+}
+
+const caneCorso = {
+    title: 'Cane Corso',
+    description: 'Video project for integrating video and illustrations together',
+    typeOfWork: 'Personal Project',
+    galleryUrl: 'https://flic.kr/s/aHsmV4Ein4',
+    image: '/assets/images/projects/designProjects/caneCorso.jpg',
+    webpImage: '/assets/images/projects/designProjects/caneCorso.webp',
+    key: 2
+}
+
+const chamili = {
+    title: 'Chamili',
+    description: "Mascot design for client's clothing line",
+    typeOfWork: 'Mascot',
+    galleryUrl: 'https://flic.kr/s/aHsmV4KQwd',
+    image: '/assets/images/projects/designProjects/chamili.jpg',
+    webpImage: '/assets/images/projects/designProjects/chamili.webp',
+    key: 3
+}
+
+const cowlick = {
+    title: 'Cowlick',
+    description: "Illustrations for client's story about a boy with peculiar hair",
+    typeOfWork: 'Book Illustration',
+    galleryUrl: 'https://flic.kr/s/aHsmV4vZKY',
+    image: '/assets/images/projects/designProjects/cowlick.jpg',
+    webpImage: '/assets/images/projects/designProjects/cowlick.webp',
+    key: 4
+}
+
+const doge = {
+    title: 'Where i belong',
+    description: "Illustrations for client's story about a dog searching for where he belongs",
+    typeOfWork: 'Book Illustration',
+    galleryUrl: 'https://flic.kr/s/aHsmV4M48U',
+    image: '/assets/images/projects/designProjects/doge.jpg',
+    webpImage: '/assets/images/projects/designProjects/doge.webp',
+    key: 5
+}
+
+const emojis = {
+    title: 'Emojis',
+    description: "Project creating alternative emoji designs",
+    typeOfWork: 'Personal Project',
+    galleryImgUrl: 'https://flic.kr/p/2kJ8uVo',
+    image: '/assets/images/projects/designProjects/emojis.jpg',
+    webpImage: '/assets/images/projects/designProjects/emojis.webp',
+    key: 6
+}
+
+const gekeerah = {
+    title: 'Gekeerah',
+    description: "Illustrations for client's inclusive rewriting of Cinderella",
+    typeOfWork: 'Book Illustration',
+    galleryUrl: 'https://flic.kr/s/aHsmV4LveZ',
+    image: '/assets/images/projects/designProjects/gekeerah.jpg',
+    webpImage: '/assets/images/projects/designProjects/gekeerah.webp',
+    key: 7
+}
+
+const gitemtaylor = {
+    title: 'Gitem Taylor',
+    description: "Album art design for artist 'GitemTaylor'",
+    typeOfWork: 'Album Art',
+    galleryImgUrl: 'https://flic.kr/p/2kQfyJ7',
+    image: '/assets/images/projects/designProjects/gitemtaylor.jpg',
+    webpImage: '/assets/images/projects/designProjects/gitemtaylor.webp',
+    key: 8
+}
+
+const iTwar = {
+    title: 'I-Twar',
+    description: "Album art designs for UK artist 'i-twar'",
+    typeOfWork: 'Album Art',
+    galleryUrl: 'https://flic.kr/s/aHsmV4FsKW',
+    image: '/assets/images/projects/designProjects/iTwar.jpg',
+    webpImage: '/assets/images/projects/designProjects/iTwar.webp',
+    key: 9
+}
+
+const marchHare = {
+    title: 'March Hare',
+    description: "Mascot design of 'The March Hare' for client",
+    typeOfWork: 'Mascot',
+    galleryImgUrl: 'https://flic.kr/p/2kGJP1C',
+    image: '/assets/images/projects/designProjects/marchHare.jpg',
+    webpImage: '/assets/images/projects/designProjects/marchHare.webp',
+    key: 10
+}
+
+const miranda = {
+    title: 'Miranda',
+    description: "Illustrations for client's story about a marshmallow trying to escape her fate of being eaten",
+    typeOfWork: 'Book Illustration',
+    galleryUrl: 'https://flic.kr/s/aHsmV4keYH',
+    image: '/assets/images/projects/designProjects/miranda.jpg',
+    webpImage: '/assets/images/projects/designProjects/miranda.webp',
+    key: 11
+}
+
+const stanLee = {
+    title: 'Stan Lee',
+    description: "Tribute illustration for the late Stan Lee",
+    typeOfWork: 'Personal Project',
+    galleryImgUrl: 'https://flic.kr/p/2kQfyJc',
+    image: '/assets/images/projects/designProjects/stanLee.jpg',
+    webpImage: '/assets/images/projects/designProjects/stanLee.webp',
+    key: 12
+}
+
+const stephenHawking = {
+    title: 'Stephen Hawking',
+    description: "Tribute illustration for the late Stephen Hawking",
+    typeOfWork: 'Personal Project',
+    galleryImgUrl: 'https://flic.kr/p/2kGJNZA',
+    image: '/assets/images/projects/designProjects/stephenHawking.jpg',
+    webpImage: '/assets/images/projects/designProjects/stephenHawking.webp',
+    key: 13
+}
+
+const unsuprvsd = {
+    title: "Unsuprvsd",
+    description: "Instagram Promo illustration for Unsuprvsd (personal clothing brand)",
+    typeOfWork: 'Personal Project',
+    galleryUrl: 'https://flic.kr/s/aHsmV4MYRk',
+    image: '/assets/images/projects/designProjects/unsuprvsd.jpg',
+    webpImage: '/assets/images/projects/designProjects/unsuprvsd.webp',
+    key: 14
+}
+
+const triads = {
+    title: "Triads",
+    description: "Exploring different culture and subculture beauty standards 3 girls at a time",
+    typeOfWork: 'Personal Project',
+    galleryUrl: 'https://flic.kr/s/aHsmUEMurF',
+    image: '/assets/images/projects/designProjects/triads.jpg',
+    webpImage: '/assets/images/projects/designProjects/triads.webp',
+    key: 15
+}
+
+const turtleWarriors = {
+    title: "Turtle Warriors",
+    description: "Board Game assets illustrations for client",
+    typeOfWork: 'Game',
+    galleryUrl: 'https://flic.kr/s/aHsmUESzHn',
+    image: '/assets/images/projects/designProjects/turtleWarriors.jpg',
+    webpImage: '/assets/images/projects/designProjects/turtleWarriors.webp',
+    key: 16
+}
+
+const urbanAmir = {
+    title: "Urban Amir",
+    description: "Designs for client's clothing line dedicated to raising autism awareness",
+    typeOfWork: 'Clothing',
+    galleryUrl: 'https://flic.kr/s/aHsmV4ibgX',
+    image: '/assets/images/projects/designProjects/urbanAmir.jpg',
+    webpImage: '/assets/images/projects/designProjects/urbanAmir.webp',
+    key: 17
+}
+
+const yakuza = {
+    title: "Yakuza",
+    description: "Video project animating with Krita",
+    typeOfWork: 'Personal Project',
+    galleryUrl: 'https://flic.kr/s/aHsmV4Einp',
+    image: '/assets/images/projects/designProjects/yakuza.jpg',
+    webpImage: '/assets/images/projects/designProjects/yakuza.webp',
+    key: 18
+}
+
+// reverse array to ensure last projects added to array appear on top
+export const featured_projects = [urbanAmir, psike, gitemtaylor, iTwar, marchHare, miranda, caneCorso, chamili, cowlick, doge, gekeerah, stanLee, stephenHawking, turtleWarriors, yakuza, triads, emojis, unsuprvsd, bowsette].reverse()
